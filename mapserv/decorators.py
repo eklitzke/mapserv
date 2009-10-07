12cbca7bcf4d5d62354553155a5662c59709986d
Evan Klitzke <evan@eklitzke.org>
2009-10-06 20:38:14 -0700

    enforce kwargs constraints more intelligently

---
+++ mapserv/decorators.py
@@ -0,0 +1,22 @@
+import functools
+
+def restrict_kw(*allowed):
+	"""Restrict the kwargs a function can take. Example usage:
+
+	@restrict_kw('foo, 'bar')
+	def myfunc(*args, **kwargs):
+		...
+	
+	If any kwargs other than 'foo' or 'bar' are sent to myfunc, then an
+	AssertionError will be raised.
+	"""
+	allowed_set = set(allowed)
+	def inner_decorator(func):
+		@functools.wraps(func)
+		def closure(*args, **kwargs):
+			extra_kw = set(kwargs.keys()) - allowed_set
+			if extra_kw:
+				raise AssertionError("Got extra kwargs %s, only %s allowed" % (list(extra_kw), list(allowed)))
+			return func(*args, **kwargs)
+		return closure
+	return inner_decorator
--- mapserv/lyric/expression.py
+++ mapserv/lyric/expression.py
@@ -1,7 +1,10 @@
 import mapserv.query.util
 from mapserv.interfaces.query import ttypes
+from mapserv.decorators import restrict_kw
 
 def make_clause(exprs, orderby=None, limit=None, offset=None):
+	"""Make the WHERE/ORDER BY/LIMIT part of a SELECT, UPDATE, or DELETE query.
+	"""
 	assert exprs, 'Must supply exprs to make_clause! (got: %r)' % (exprs,)
 	assert all(isinstance(expr, ttypes.Comparison) for expr in exprs)
 
@@ -12,22 +15,25 @@ def make_clause(exprs, orderby=None, limit=None, offset=None):
 		else:
 			kwargs['orderby'] = [orderby]
 	if limit is not None:
-		kwargs['limit'] == limit
+		kwargs['limit'] = limit
 		if offset:
-			kwargs['offset'] == offset
+			kwargs['offset'] = offset
 	
 	return ttypes.QueryClause(**kwargs)
 
+@restrict_kw('orderby', 'limit', 'offset')
 def select(*args, **kwargs):
 	clause = make_clause(args, orderby=kwargs.get('orderby'), limit=kwargs.get('limit'), offset=kwargs.get('offset'))
 	return ttypes.Query(variety=ttypes.QueryType.SELECT, clause=clause)
 
-def update(limit=None, *args):
-	clause = make_clause(args, limit=limit)
+@restrict_kw('limit')
+def update(*args, **kwargs):
+	clause = make_clause(args, limit=kwargs.get('limit'))
 	return ttypes.Query(variety=ttypes.QueryType.UPDATE, clause=clause)
 
-def delete(limit=None, *args):
-	clause = make_clause(args, limit=limit)
+@restrict_kw('limit')
+def delete(*args, **kwargs):
+	clause = make_clause(args, limit=kwargs.get('limit'))
 	return ttypes.Query(variety=ttypes.QueryType.DELETE, clause=clause)
 
 def insert(table, **kwargs):
